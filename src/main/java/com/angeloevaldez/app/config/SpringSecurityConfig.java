28df21a41c55a16062ff2294de5ee4181331b516
Angelo E. Valdez <angeloe.valdez@gmail.com>
2017-09-25 23:00:43 -0400

    test jenkins

---
+++ src/main/java/com/angeloevaldez/app/config/SpringSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.angeloevaldez.app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private AccessDeniedHandler accessDeniedHandler;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER")
+		.and()
+		.withUser("admin").password("password").roles("ADMIN");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+		.authorizeRequests()
+			.antMatchers("/", "/home", "/about").permitAll()
+			.antMatchers("/admin/**").hasAnyRole("ADMIN")
+			.antMatchers("/user/**").hasAnyRole("USER")
+			.anyRequest().authenticated()
+		.and()
+		.formLogin()
+			.loginPage("/login").permitAll()
+		.and()
+		.logout().permitAll()
+		.and()
+		.exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+	}
+
+}
